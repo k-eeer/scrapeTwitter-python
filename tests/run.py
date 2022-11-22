55f0e38669608474bda6c26c2b81a900f654448d
k-eeer <37653119+k-eeer@users.noreply.github.com>
2022-11-22 23:20:22 +0800

    Add files via upload

---
+++ tests/run.py
@@ -0,0 +1,11 @@
+import pytest
+import sys
+
+def main():
+    # extract your arg here
+    print('Extracted arg is ==> %s' % sys.argv[2])
+    pytest.main([sys.argv[1]])
+
+if __name__ == '__main__':
+    main()
+
--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,16 @@
+import snscrape.modules.twitter as sntwitter
+from datetime import datetime
+import os,sys,mysql.connector
+from scrape import Scraper 
 
+def test_user_scrape():
+	#arrange:準備變數值
+	expected=sys.argv[2]
+	result_list=[]
+
+	#act:執行待測部份
+	result_list=(Scraper.userScrape())	
+	#assert:確認結果是否符合預期
+	for element in result_list:
+		result=(element[2].split('/'))[3]
+		assert result==expected
