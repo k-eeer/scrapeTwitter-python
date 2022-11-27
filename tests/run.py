e566a5d3aa1855312c8281efb21eec1d25739121
k-eeer <37653119+k-eeer@users.noreply.github.com>
2022-11-27 09:39:31 +0800

    Add files via upload

--- tests/run.py
+++ tests/run.py
@@ -1,6 +1,8 @@
 import pytest
 import sys
 
+#本script用於協助擷取參數，用法：python3 run.py code.py argv2 argv3 argv4 ...
+
 def main():
     # extract your arg here
     print('Extracted arg is ==> %s' % sys.argv[2:])
