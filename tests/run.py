2c78de814a6147c953b3e9737536be4181416cd8
k-eeer <37653119+k-eeer@users.noreply.github.com>
2022-11-27 09:20:32 +0800

    Update run.py

--- tests/run.py
+++ tests/run.py
@@ -1,12 +1,12 @@
 import pytest
 import sys
 
-#本檔提供後續script抓取參數，例：python3 run.py test.py <argv>
+#本檔提供後續script抓取參數，例：python3 run.py test.py <argv2> <argv3> <argv4> ...
 
 
 def main():
     # extract your arg here
-    print('Extracted arg is ==> %s' % sys.argv[2])
+    print('Extracted arg is ==> %s' % sys.argv[2:])
     pytest.main([sys.argv[1]])
 
 if __name__ == '__main__':
